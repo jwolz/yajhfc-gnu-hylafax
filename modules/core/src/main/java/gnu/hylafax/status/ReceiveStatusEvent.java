dd899db8e643004bdffccf075f489ab43cea07a6
sjardine <sjardine@070a802e-e62f-0410-a497-88e9c6677881>
2008-02-28 23:58:54 +0000

    Finished rough work on ReceiveStatusEvent class.  Still needs more testing.

--- modules/core/src/main/java/gnu/hylafax/status/ReceiveStatusEvent.java
+++ modules/core/src/main/java/gnu/hylafax/status/ReceiveStatusEvent.java
@@ -35,47 +35,6 @@ public class ReceiveStatusEvent extends BaseStatusEvent {
 
     private static final Log log = LogFactory.getLog(ReceiveStatusEvent.class);
 
-    private String sender = null;
-    private String commId = null;
-
-    public ReceiveStatusEvent(Event event, String serverStr) {
-	super(event, serverStr);
-	if (serverStr != null) {
-	    String details = (serverStr.split("RECV FAX: ")[1]).trim()
-		    .toUpperCase();
-
-	    // Parse the details.
-	    if (details.startsWith("CALL")) {
-		description = details;
-		return;
-	    }
-
-	    if (details.startsWith("SESSION STARTED")) {
-		String tmp = details.substring(details.indexOf(" (COM ") + 6,
-			details.indexOf("), ")).trim();
-		if (!tmp.equals(""))
-		    commId = tmp;
-		return;
-	    }
-
-	    if (details.startsWith("FROM")) {
-		String tmp = details.substring(details.indexOf("FROM ") + 5,
-			details.indexOf(" (COM ")).trim();
-		if (!tmp.equals(""))
-		    sender = tmp;
-
-		tmp = details.substring(details.indexOf(" (COM ") + 6,
-			details.indexOf("), ")).trim();
-		if (!tmp.equals(""))
-		    commId = tmp;
-
-		System.err.println(sender + " | " + commId);
-	    }
-	    // log.debug("Receive Event Details: " + details);
-
-	}
-    }
-
     public static void main(String[] args) {
 	try {
 	    BasicConfigurator.configure();
@@ -87,11 +46,250 @@ public class ReceiveStatusEvent extends BaseStatusEvent {
 		Event event = Event.getEvent(eventId);
 
 		StatusEvent statusEvent = new ReceiveStatusEvent(event, line);
-		// System.out.println(statusEvent.toString());
+		System.err.println(statusEvent.toString());
 	    }
 	} catch (Exception e) {
 	    e.printStackTrace();
 	}
     }
 
+    private String bitRate = null;
+
+    private String commId = null;
+
+    private String dataFormat = null;
+
+    private String pageLength = null;
+
+    private Integer pageNumber = null;
+
+    private Integer pageSeconds = null;
+
+    private String scanlineTime = null;
+
+    private String sender = null;
+
+    private Integer totalPages = null;
+
+    private String verticalResolution = null;
+
+    public ReceiveStatusEvent(Event event, String serverStr) {
+	super(event, serverStr);
+	if (serverStr != null) {
+	    String details = null;
+	    try {
+		details = (serverStr.split("RECV FAX: ")[1]).trim()
+			.toUpperCase();
+	    } catch (Exception e) {
+		log.warn("Cannot parse server string: " + serverStr
+			+ "; Exception: " + e.getMessage());
+	    }
+	    if (details != null) {
+		// Parse the details.
+		if (event == Event.RECV_BEGIN || event == Event.RECV_END) {
+		    description = details;
+		} else {
+		    sender = parseSender(details);
+		    commId = parseCommId(details);
+		    if (event == Event.RECV_START) {
+			setParams(details);
+		    } else if (event == Event.RECV_PAGE) {
+			// Parse the page number.
+			pageNumber = parsePageNumber(details);
+			pageSeconds = parsePageSeconds(details);
+			setParams(details);
+		    } else if (event == Event.RECV_DOC) {
+			totalPages = parseTotalPages(details);
+			pageSeconds = parsePageSeconds(details);
+		    }
+		}
+	    }
+	}
+    }
+
+    public String getBitRate() {
+	return bitRate;
+    }
+
+    public String getCommId() {
+	return commId;
+    }
+
+    public String getDataFormat() {
+	return dataFormat;
+    }
+
+    public String getPageLength() {
+	return pageLength;
+    }
+
+    public Integer getPageNumber() {
+	return pageNumber;
+    }
+
+    public Integer getPageSeconds() {
+	return pageSeconds;
+    }
+
+    public String getScanlineTime() {
+	return scanlineTime;
+    }
+
+    public String getSender() {
+	return sender;
+    }
+
+    public Integer getTotalPages() {
+	return totalPages;
+    }
+
+    public String getVerticalResolution() {
+	return verticalResolution;
+    }
+
+    private String parseCommId(String details) {
+	try {
+	    String tmp = details.substring(details.indexOf(" (COM ") + 6,
+		    details.indexOf("), ")).trim();
+	    if (!tmp.equals(""))
+		return tmp;
+	} catch (Exception e) {
+	    log.warn(e.getMessage(), e);
+	}
+	return null;
+    }
+
+    private Integer parsePageNumber(String details) {
+	try {
+	    return new Integer(Integer.parseInt(details.substring(details
+		    .indexOf("), PAGE ") + 8, details.indexOf(" IN "))));
+	} catch (Exception e) {
+	    log.warn("Cannot parse total pages: " + details + "; Exception: "
+		    + e.getMessage());
+	}
+	return null;
+    }
+
+    private Integer parsePageSeconds(String details) {
+	String[] time = null;
+	try {
+	    if (event == Event.RECV_PAGE) {
+		time = details.substring(details.indexOf(" IN ") + 4,
+			details.lastIndexOf(" <")).trim().split(":");
+	    } else if (event == Event.RECV_DOC) {
+		time = details.substring(details.indexOf(" PAGES IN ") + 10,
+			details.indexOf(", FILE")).trim().split(":");
+	    }
+	} catch (Exception e) {
+	    log.warn("Cannot parse time string: " + details + "; Exception: "
+		    + e.getMessage());
+	}
+
+	try {
+	    if (time != null && time.length == 3) {
+		int seconds = Integer.parseInt(time[0]) * 60 * 60;
+		seconds += Integer.parseInt(time[1]) * 60;
+		seconds += Integer.parseInt(time[2]);
+		return new Integer(seconds);
+	    }
+	    throw new Exception("Time String is null or wrong size");
+	} catch (Exception e) {
+	    log.warn("Cannot calculate time: " + details + "; Exception: "
+		    + e.getMessage());
+	}
+	return null;
+    }
+
+    private String parseSender(String details) {
+	String result = null;
+	try {
+	    if (event == Event.RECV_START) {
+		result = details.substring(details.indexOf("TSI \"") + 5,
+			details.indexOf("\" <")).trim();
+	    } else if (event == Event.RECV_PAGE || event == Event.RECV_DOC) {
+		result = details.substring(details.indexOf("FROM ") + 5,
+			details.indexOf(" (COM ")).trim();
+	    }
+	} catch (Exception e) {
+	    log.warn("Cannot parse sender: " + details + "; Exception: "
+		    + e.getMessage());
+	}
+	if (result != null) {
+	    result = result.trim();
+	    return result.equals("") ? null : result;
+	}
+	return null;
+    }
+
+    private Integer parseTotalPages(String details) {
+	try {
+	    return new Integer(Integer.parseInt(details.substring(details
+		    .indexOf("), ") + 3, details.indexOf(" PAGES IN "))));
+	} catch (Exception e) {
+	    log.warn("Cannot parse total pages: " + details + "; Exception: "
+		    + e.getMessage());
+	}
+	return null;
+    }
+
+    private String prepStr(String str) {
+	if (str != null) {
+	    String tmp = str.trim();
+	    if (!tmp.equals("")) {
+		return tmp;
+	    }
+	}
+	return null;
+    }
+
+    private String errorMsg(String message, String info, Exception e) {
+	String result = "";
+	if (prepStr(message) != null) {
+	    result += prepStr(message);
+	    if (prepStr(info) != null) {
+		result += ": " + prepStr(info);
+		if (e != null && prepStr(e.getMessage()) != null) {
+		    result += "; Exception: " + prepStr(e.getMessage());
+		}
+	    }
+	}
+	return result;
+    }
+
+    private void setParams(String details) {
+	String info = null;
+	try {
+	    info = details.substring(details.lastIndexOf("<") + 1, details
+		    .lastIndexOf(">"));
+	} catch (Exception e) {
+	    log.warn("Cannot Parse Info From Details: " + details
+		    + "; Exception: " + e.getMessage());
+	    return;
+	}
+
+	if (info != null) {
+	    try {
+		log.debug("Event Info:\t" + info);
+		String[] params = info.split(", ");
+		pageLength = prepStr(params[0]);
+		verticalResolution = prepStr(params[1]);
+		dataFormat = prepStr(params[2]);
+		bitRate = prepStr(params[3]);
+		scanlineTime = prepStr(params[4]);
+	    } catch (Exception e) {
+		log.warn("Cannot Parse Info: " + info + ", " + e.getMessage());
+	    }
+	}
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see gnu.hylafax.status.BaseStatusEvent#toString()
+     */
+    public String toString() {
+	String result = super.toString();
+	// TODO Do more.
+	return result;
+    }
 }
